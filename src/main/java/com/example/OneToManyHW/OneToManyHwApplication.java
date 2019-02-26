bdfc0f5aa61bea24782ddd2e401eb24254152258
lacey <hiu@example.com>
2019-02-26 15:35:56 +0000

    add initial files

---
+++ src/main/java/com/example/OneToManyHW/OneToManyHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.OneToManyHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToManyHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OneToManyHW/OneToManyHwApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.OneToManyHW;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OneToManyHwApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
